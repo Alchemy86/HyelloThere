12c5d39b3e81de6dca5da632f5f8756be1612ab6
alchemy86 <codebyexample@gmail.com>
2024-03-31 10:46:11 +0100

    Icons added. Basic navigation implemented - mobile included

---
+++ components/Sidebar.tsx
@@ -0,0 +1,51 @@
+'use client';
+import { sidebarLinks } from '@/constants';
+import { cn } from '@/lib/utils';
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation'
+import React from 'react'
+
+
+const Sidebar = () => {
+  const pathNmae = usePathname();
+  return (
+    <section className='sticky left-0 top-0 flex h-screen w-fit flex-col 
+    justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]'>
+
+      <div className='flex flex-1 flex-col gap-6'>
+        {sidebarLinks.map((link) => {
+          const isActive = pathNmae === link.route;
+
+          return (
+            <Link
+              href={link.route}
+              key = {link.lable}
+              className={cn('flex gap-4 items-center p-4 rounded-lg justify-start', {
+                'bg-blue-1': isActive // Blue 1 style if is active link
+              })}
+            >
+              
+
+              <Image 
+                src={link.imgUrl}
+                alt={link.lable}
+                width={24}
+                height={24}
+              />
+
+              <p className='font-semibold'>
+                  {link.lable}
+              </p>
+
+            </Link>
+
+          )
+        })}
+      </div>
+
+    </section>
+  )
+}
+
+export default Sidebar
---
+++ constants/index.ts
@@ -0,0 +1,26 @@
+export const sidebarLinks = [
+    {
+        lable: 'Home',
+        route: '/',
+        imgUrl: '/icons/home.svg'
+    },
+    {
+        lable: 'Upcoming',
+        route: '/upcoming',
+        imgUrl: '/icons/upcoming.svg'
+    },
+    {
+        lable: 'Previous',
+        route: '/previous',
+        imgUrl: '/icons/previous.svg'
+    },
+    {
+        lable: 'Recordings',
+        route: '/recordings',
+        imgUrl: '/icons/recordings.svg'
+    },{
+        lable: 'Personal Room',
+        route: '/personal-room',
+        imgUrl: '/icons/add-personal.svg'
+    }
+]
