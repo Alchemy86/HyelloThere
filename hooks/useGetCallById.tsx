510d6e6cb2952111f6a6daf3e6cb4d0026877c29
alchemy86 <codebyexample@gmail.com>
2024-03-31 19:39:16 +0100

    alerts, metting config

---
+++ hooks/useGetCallById.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk';
+import { toast } from '@/components/ui/use-toast';
+
+export const useGetCallById = (id: string | string[]) => {
+  const [call, setCall] = useState<Call>();
+  const [isCallLoading, setIsCallLoading] = useState(true);
+
+  const client = useStreamVideoClient();
+
+  useEffect(() => {
+    if (!client) return;
+    
+    const loadCall = async () => {
+      try {
+        // https://getstream.io/video/docs/react/guides/querying-calls/#filters
+        const { calls } = await client.queryCalls({ filter_conditions: { id } });
+
+        if (calls.length > 0) setCall(calls[0]);
+
+        setIsCallLoading(false);
+      } catch (error) {
+        toast({ title: 'Failed to load call'} );
+        console.error(error);
+        setIsCallLoading(false);
+      }
+    };
+
+    loadCall();
+  }, [client, id]);
+
+  return { call, isCallLoading };
+};
