16747d3263ae1463595ef7dab75cae51029807d3
alchemy86 <codebyexample@gmail.com>
2024-03-31 16:14:12 +0100

    main page cards - basic setup

---
+++ components/MeetingTypeList.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import Image from 'next/image';
+import React, { useState } from 'react'
+import HomeCard from './HomeCard';
+import { useRouter } from 'next/navigation';
+
+const MeetingTypeList = () => {
+    const [meetingState, setMeetingState] = useState<'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined>()
+    const router = useRouter();
+
+  return (
+        <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
+
+        <HomeCard 
+            img='/icons/add-meeting.svg' 
+            title='New Meeting'
+            description='Start an instant meeting' 
+            className = 'bg-orange-1'
+            handleClick={() => setMeetingState('isJoiningMeeting')}/>
+        <HomeCard 
+            img='/icons/schedule.svg' 
+            title='Schedule Meeting'
+            description='Plan your meeting' 
+            className = 'bg-blue-1'
+            handleClick={() => setMeetingState('isScheduleMeeting')}/>
+        <HomeCard 
+            img='/icons/recordings.svg' 
+            title='View Recordings'
+            description='Check out your recordings' 
+            className = 'bg-purple-1'
+            handleClick={() => router.push('/recordings')}/>
+        <HomeCard 
+            img='/icons/join-meeting.svg' 
+            title='JOin Meeting'
+            description='Via invitation link' 
+            className = 'bg-yellow-1'
+            handleClick={() => setMeetingState('isJoiningMeeting')}/>
+    </section>
+  )
+}
+
+export default MeetingTypeList
