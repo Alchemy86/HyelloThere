99667bd9b524ea50ab3337d7444ffca8fa292ad9
aaron <aaron@regtransfers.co.uk>
2024-04-17 11:53:56 +0100

    config

--- components/MavaHeader.tsx
+++ components/MavaHeader.tsx
@@ -10,8 +10,7 @@ const Mava = () => (
         widget-version="v2" 
         id="MavaWebChat" 
         enable-sdk="true" 
-        data-token="fa51bf79dafb75d99f19c09f4e2b9a76b6f4a896ab1f78e552afed2cafd64099"/>
-
+        data-token={process.env.MAVA_TOKEN}/>
 
   );
 
