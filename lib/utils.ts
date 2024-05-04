cbfc20494a3124672173aa5a49fa4aac16f914fc
aaron <aaron@regtransfers.co.uk>
2024-05-05 00:22:08 +0100

    super close

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,14 +8,15 @@ export function cn(...inputs: ClassValue[]) {
 export const getValidSubdomain = (host?: string | null) => {
   let subdomain: string | null = null;
   if (!host && typeof window !== 'undefined') {
-    // On client side, get the host from window
+    // On the client side, get the host from window
     host = window.location.host;
   }
-  if (host && host.includes('.')) {
-    const candidate = host.split('.')[0];
-    if (candidate && !candidate.includes('localhost')) {
-      // Valid candidate
-      subdomain = candidate;
+  if (host && host.includes('.') && !host.startsWith('www') && !host.startsWith('localhost')) {
+    // Check if host contains a dot (.) and not starting with 'www' or 'localhost'
+    const parts = host.split('.');
+    if (parts.length > 2) {
+      // Ensure host consists of more than two parts to confirm it's a valid subdomain
+      subdomain = parts[0];
     }
   }
   return subdomain;
