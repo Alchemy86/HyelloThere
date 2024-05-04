6cc3e719c08d596b0704e4ec8fc6a7d8af11e6a8
aaron <aaron@regtransfers.co.uk>
2024-05-05 00:40:59 +0100

    revert

--- next.config.mjs
+++ next.config.mjs
@@ -7,35 +7,6 @@ const nextConfig = {
       },
     ],
   },
-
-  // Define custom rewrites
-  // Ensure to use 'rewrites' from 'next/dist/next-server/server/config'
-  async rewrites() {
-    return [
-      {
-        // Rewrite rule for the root path of booking.hyello.co.uk
-        source: '/',
-        has: [
-          {
-            type: 'host',
-            value: 'booking.hyello.co.uk',
-          },
-        ],
-        destination: '/booking', // Point to the 'pages/booking/index.js' file
-      },
-      {
-        // Rewrite rule for all paths beyond the root
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'booking.hyello.co.uk',
-          },
-        ],
-        destination: '/booking/:path*', // Point to the 'pages/booking' directory
-      },
-    ];
-  },
 };
 
 export default nextConfig;
