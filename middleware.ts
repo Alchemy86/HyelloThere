642c0d7ab4552fe752b313be72f87986c3bbbb4b
aaron <aaron@regtransfers.co.uk>
2024-05-05 00:45:19 +0100

    work a bit p[lease

--- middleware.ts
+++ middleware.ts
@@ -25,3 +25,23 @@ export const config = {
 
 // RegExp for public files
 const PUBLIC_FILE = /\.(.*)$/; // Files
+
+export async function middleware(req: NextRequest) {
+  // Clone the URL
+  const url = req.nextUrl.clone();
+
+  // Skip public files
+  if (PUBLIC_FILE.test(url.pathname) || url.pathname.includes('_next')) return NextResponse.next();
+
+  const host = req.headers.get('host');
+  const subdomain = getValidSubdomain(host);
+  if (subdomain) {
+    // Subdomain available, rewriting
+    console.log(`>>> Rewriting: ${url.pathname} to /${subdomain}${url.pathname}`);
+    url.pathname = `/${subdomain}${url.pathname}`;
+    return NextResponse.rewrite(url);
+  }
+
+  // No subdomain detected, proceed with the original request
+  return NextResponse.next();
+}
