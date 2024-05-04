7f8e043f59725dd210a4b4f6ec94d5abb3f2d13a
aaron <aaron@regtransfers.co.uk>
2024-05-05 00:53:44 +0100

    MW change

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
 import { getValidSubdomain } from './lib/utils';
-
 
 const protectedRoute = createRouteMatcher([
   '/',
@@ -14,29 +12,23 @@ const protectedRoute = createRouteMatcher([
   '/booking(.*)'
 ]);
 
-export default clerkMiddleware((auth, req) => {
-  if (protectedRoute(req)) auth().protect();
-});
-
-export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
-};
-
-
-// RegExp for public files
-const PUBLIC_FILE = /\.(.*)$/; // Files
-
-export async function middleware(req: NextRequest) {
+const clerkMiddlewareInstance = clerkMiddleware((auth, req) => {
   // Clone the URL
   const url = req.nextUrl.clone();
 
   // Skip public files
+  const PUBLIC_FILE = /\.(.*)$/; // Files
   if (PUBLIC_FILE.test(url.pathname) || url.pathname.includes('_next')) return NextResponse.next();
 
+  // Check if the request matches a protected route
+  if (protectedRoute(req)) auth().protect();
+
+  // Extract subdomain from host
   const host = req.headers.get('host');
   const subdomain = getValidSubdomain(host);
+
+  // Rewrite URL if subdomain exists
   if (subdomain) {
-    // Subdomain available, rewriting
     console.log(`>>> Rewriting: ${url.pathname} to /${subdomain}${url.pathname}`);
     url.pathname = `/${subdomain}${url.pathname}`;
     return NextResponse.rewrite(url);
@@ -44,4 +36,6 @@ export async function middleware(req: NextRequest) {
 
   // No subdomain detected, proceed with the original request
   return NextResponse.next();
-}
+});
+
+export default clerkMiddlewareInstance;
