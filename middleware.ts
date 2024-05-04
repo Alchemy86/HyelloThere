f5fd4dacfa1afe7452db63ca20eb34d41c83a5f9
aaron <aaron@regtransfers.co.uk>
2024-05-05 00:13:07 +0100

    please work

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,8 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { getValidSubdomain } from './lib/utils';
+
 
 const protectedRoute = createRouteMatcher([
   '/',
@@ -16,3 +20,26 @@ export default clerkMiddleware((auth, req) => {
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
+
+
+
+// RegExp for public files
+const PUBLIC_FILE = /\.(.*)$/; // Files
+
+export async function middleware(req: NextRequest) {
+  // Clone the URL
+  const url = req.nextUrl.clone();
+
+  // Skip public files
+  if (PUBLIC_FILE.test(url.pathname) || url.pathname.includes('_next')) return;
+
+  const host = req.headers.get('host');
+  const subdomain = getValidSubdomain(host);
+  if (subdomain) {
+    // Subdomain available, rewriting
+    console.log(`>>> Rewriting: ${url.pathname} to /${subdomain}${url.pathname}`);
+    url.pathname = `/${subdomain}${url.pathname}`;
+  }
+
+  return NextResponse.rewrite(url);
+}
